d3bb8826acafc0c0ccac9e5ba350c4eb5610f0c2
hruday-dev <thirumalahrudayboda@gmail.com>
2025-10-04 16:15:02 +0530

    finished mobile and desktop design

--- js/script.js
+++ js/script.js
@@ -1,27 +1,29 @@
 const sharebtn = document.querySelector('button');
 const socialMedia = document.createElement('div');
 const sharediv = document.querySelector(".sharebutton");
-const mediaQuery = window.matchMedia('(max-width: 600px)');
 socialMedia.setAttribute('class', 'socialmedia')
 socialMedia.innerHTML=`
 <p class="show">SHARE</p>
+<div class="drop"></div>
 <img class="show" src="./images/icon-facebook.svg" alt="icon-facebook">
 <img class="show" src="./images/icon-twitter.svg" alt="icon-twitter">
 <img class="show" src="./images/icon-pinterest.svg" alt="icon-twitter">  
 `;
 sharebtn.before(socialMedia);
 socialMedia.style.display='none';
+sharediv.style.boxShadow = "none";
 
 sharebtn.addEventListener("click", () => {
     sharebtn.classList.toggle('active');
     if (sharebtn.classList.contains('active')) {
         sharebtn.style.display = 'flex';
         socialMedia.style.display = 'flex'
-        if (mediaQuery.matches){
-            sharediv.style.backgroundColor = 'hsl(217, 19%, 35%)';
-        }
+        sharediv.style.backgroundColor = 'hsl(217, 19%, 35%)';
+        sharediv.style.boxShadow = "5px 5px 5px hsl(210, 46%, 95%)";
+        
     }else{
         socialMedia.style.display = 'none';
         sharediv.style.backgroundColor = 'transparent';
+        sharediv.style.boxShadow = "none";
     }
 });
