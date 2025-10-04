b88c2af125740db66550e7d2a7741aa095183741
hruday-dev <thirumalahrudayboda@gmail.com>
2025-10-04 13:39:56 +0530

    completed mobile responsive design adjustments

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
 const sharebtn = document.querySelector('button');
-const sharediv = document.querySelector('.sharebutton')
-sharebtn.innerHTML=`
+const socialMedia = document.createElement('div');
+const sharediv = document.querySelector(".sharebutton");
+const mediaQuery = window.matchMedia('(max-width: 600px)');
+socialMedia.setAttribute('class', 'socialmedia')
+socialMedia.innerHTML=`
 <p class="show">SHARE</p>
 <img class="show" src="./images/icon-facebook.svg" alt="icon-facebook">
 <img class="show" src="./images/icon-twitter.svg" alt="icon-twitter">
-<img class="show" src="./images/icon-pinterest.svg" alt="icon-twitter">
+<img class="show" src="./images/icon-pinterest.svg" alt="icon-twitter">  
 `;
-
-const show = document.querySelector('.show')
-show.style.display = 'none';
+sharebtn.before(socialMedia);
+socialMedia.style.display='none';
 
 sharebtn.addEventListener("click", () => {
     sharebtn.classList.toggle('active');
     if (sharebtn.classList.contains('active')) {
-       sharebtn.style.display = 'flex'; // or 'block' as you need
-    } 
+        sharebtn.style.display = 'flex';
+        socialMedia.style.display = 'flex'
+        if (mediaQuery.matches){
+            sharediv.style.backgroundColor = 'hsl(217, 19%, 35%)';
+        }
+    }else{
+        socialMedia.style.display = 'none';
+        sharediv.style.backgroundColor = 'transparent';
+    }
 });
